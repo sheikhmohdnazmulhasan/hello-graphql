fa9094a6dc81093a9a13d77182115b67dba32442
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2025-01-15 15:03:30 +0600

    categories type created in schema

--- src/gql/schema/index.ts
+++ src/gql/schema/index.ts
@@ -1,17 +1,23 @@
 export const typeDefs = `#graphql
   type Product {
-  id: ID!
-  name: String!
-  image: String!
-  description: String!
-  price: Float!
-  quantity: Int!
-  onStock: Boolean!
-  category: String!
+    id: ID!
+    name: String!
+    image: String!
+    description: String!
+    price: Float!
+    quantity: Int!
+    onStock: Boolean!
+    category: String!
+  },
+
+  type Category {
+    id: ID!
+    name: String
   }
 
   type Query {
     products: [Product]
     product(productId: ID!): Product
+    categories: [Category]
   }
 `;
